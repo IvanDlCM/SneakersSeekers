ff80082368aefd35240b33cac63cb3f9ebf94d11
IvanDlCM <20302060@utfv.edu.mx>
2023-03-31 18:04:42 -0600

    Actualización de archivos de imágenes y plantillas de HTML

--- app.py
+++ app.py
@@ -18,7 +18,12 @@ def inicio():
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    if not midb.is_connected():
+        midb.connect()
+    cursor = midb.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM productos limit 3')
+    muestras = cursor.fetchall()
+    return render_template('index.html', muestras=muestras)
 
 @app.route('/marcas')
 def marcas():
@@ -32,7 +37,7 @@ def obtener_producto(id):
     if not midb.is_connected():
         midb.connect()
     cursor = midb.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM productos WHERE id = {id}")
+    cursor.execute("SELECT * FROM productos WHERE id = %s", (id,))
     producto = cursor.fetchone()
     return producto
 
@@ -40,7 +45,7 @@ def obtener_relacionados(id):
     if not midb.is_connected():
         midb.connect()
     cursor = midb.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM productos WHERE id != {id}")
+    cursor.execute("SELECT * FROM productos WHERE id != %s", (id,))
     productos_relacionados = cursor.fetchall()
     return productos_relacionados
 
@@ -49,9 +54,9 @@ def productos():
     if not midb.is_connected():
         midb.connect()
     cursor = midb.cursor(dictionary=True)
-    cursor.execute('select * from productos')
+    cursor.execute('SELECT * FROM productos')
     productos = cursor.fetchall()
-    return render_template('todosProductos.html', productos=productos)
+    return render_template('productos.html', productos=productos)
 
 @app.route('/productos/<int:id>')
 def producto(id):
@@ -59,64 +64,49 @@ def producto(id):
     productos_relacionados = obtener_relacionados(id)
     return render_template('producto.html', producto=producto, productos_relacionados=productos_relacionados)
 
+@app.route('/nuevo_producto', methods=['GET', 'POST'])
+def nuevo_producto():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        imagen = request.form['imagen']
+        if (nombre !='') and (descripcion != '') and (precio != '') and (imagen != ''):
+            if not midb.is_connected():
+                midb.connect()
+            cursor = midb.cursor(dictionary=True)
+            cursor.execute("INSERT INTO productos (nombre, descripcion, precio, imagen) VALUES (%s, %s, %s, %s)", (nombre, descripcion, precio, imagen))
+            midb.commit()
+            cursor.close()
+            midb.close()
+            return redirect(url_for('productos'))
+        else:
+            return redirect(url_for('productos'))
+
 @app.route('/productos/eliminar/<int:id>', methods=['POST'])
 def eliminar_producto(id):
     if not midb.is_connected():
         midb.connect()
     cursor = midb.cursor(dictionary=True)
-    cursor.execute(f"delete from productos WHERE id = {id}")
+    cursor.execute("DELETE FROM productos WHERE id = %s", (id,))
     midb.commit()
     cursor.close()
-
     return redirect(url_for('productos'))
 
-@app.route('/productos/<int:id>/editar', methods=['GET'])
-def editar_producto(id):
-    producto = obtener_producto(id)
-    # Mostrar el formulario de actualización con los detalles del producto
-    return render_template('editar_producto.html', producto=producto)
-
-@app.route('/productos/<int:id>/guardar', methods=['POST'])
-def guardar_cambios(id):
-    if not midb.is_connected():
-        midb.connect()
-    cursor = midb.cursor(dictionary=True)
-    # Obtener los datos actualizados del formulario
+@app.route('/productos/<int:id>/actualizar', methods=['POST'])
+def actualizar(id):
     nombre = request.form['nombre']
     descripcion = request.form['descripcion']
     precio = request.form['precio']
     imagen = request.form['imagen']
-    # Actualizar el producto en la base de datos
-    cursor.execute(f"UPDATE productos SET nombre = '{nombre}', descripcion = '{descripcion}', precio = '{precio}',"
-                   f" imagen = '{imagen}' WHERE id = {id}")
-    midb.commit()
-    cursor.close()
-    midb.close()
-    # Redirigir al usuario de regreso a la vista de detalles del producto actualizado
-    return redirect(url_for('producto', id=id))
-
-
-
-@app.route('/nuevo_producto', methods=['GET', 'POST'])
-def nuevo_producto():
-    if request.method == 'POST':
-        # Obtener los datos del formulario
-        nombre = request.form['nombre']
-        descripcion = request.form['descripcion']
-        precio = request.form['precio']
-        imagen = request.form['imagen']
-
+    if (nombre !='') and (descripcion != '') and (precio != '') and (imagen != ''):
         if not midb.is_connected():
             midb.connect()
         cursor = midb.cursor(dictionary=True)
-        cursor.execute(f"INSERT INTO productos (nombre, descripcion, precio, imagen) VALUES ('{nombre}', '{descripcion}"
-                       f"', '{precio}', '{imagen}')")
+        cursor.execute("UPDATE productos SET nombre = %s, descripcion = %s, precio = %s, imagen = %s WHERE id = %s", (nombre, descripcion, precio, imagen, id))
         midb.commit()
         cursor.close()
         midb.close()
-        # Redirigir al usuario a la página de productos
         return redirect(url_for('productos'))
-
-    # Renderizar el formulario para ingresar nuevos productos
-    return render_template('nuevo_producto.html')
-
+    else:
+        return redirect(url_for('productos'))
